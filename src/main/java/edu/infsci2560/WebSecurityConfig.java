c65ad33c294f3d0b7d004d1c5ee030a8a09b67a5
J Stephanie Rose <jsr67@pitt.edu>
2017-02-18 02:25:19 +0000

    changed websecurityconfig

--- src/main/java/edu/infsci2560/WebSecurityConfig.java
+++ src/main/java/edu/infsci2560/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login")
                 .permitAll()
                 .and()
-            .logout()
+            .logout("/logout")
                 .permitAll();
     }
 
