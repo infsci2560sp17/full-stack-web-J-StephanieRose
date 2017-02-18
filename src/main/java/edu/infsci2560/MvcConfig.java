83b41a51b8c8a02f1fd6660e9d3e1eb949a12eec
J Stephanie Rose <jsr67@pitt.edu>
2017-02-18 02:45:36 +0000

    still trying to resolve the post error

--- src/main/java/edu/infsci2560/MvcConfig.java
+++ src/main/java/edu/infsci2560/MvcConfig.java
@@ -13,7 +13,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/interest").setViewName("interest");
         registry.addViewController("/login").setViewName("login");
     }
 
