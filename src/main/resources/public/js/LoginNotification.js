fb1dbeb967b17fbc7d417e2a4497101bdd40053b
J Stephanie Rose <jsr67@pitt.edu>
2017-02-24 06:47:01 +0000

    attempting some js changes resources>public

---
+++ src/main/resources/public/js/LoginNotification.js
@@ -0,0 +1,14 @@
+/*
+ * source: lipp/login-with
+ */
+
+import React from 'react'
+import Link from 'next/link'
+
+export default ({profile, redirect}) => (
+  <div className='notification'>
+    Welcome back <strong>{profile.name || profile.username}</strong>!
+    You can check your <Link href='/profile'><a>profile</a></Link> or
+    <a href={`https://login.now.sh/logout?success=${redirect}`}> logout </a>
+  </div>
+)
