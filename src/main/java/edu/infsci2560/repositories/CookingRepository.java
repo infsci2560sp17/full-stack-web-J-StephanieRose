be3b9dc06d491e125db88bcb5d0e1e1af7dd5fd5
J Stephanie Rose <jsr67@pitt.edu>
2017-03-25 23:43:45 +0000

    added CookingRepository.java

---
+++ src/main/java/edu/infsci2560/repositories/CookingRepository.java
@@ -0,0 +1,49 @@
+
+/*
+
+
+
+
+* To change this license header, choose License Headers in Project Properties.
+
+
+* To change this template file, choose Tools | Templates
+
+
+* and open the template in the editor.
+
+
+*/
+
+
+package edu.infsci2560.repositories;
+
+
+
+
+
+
+
+import edu.infsci2560.models.Cooking;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+
+
+
+
+
+
+/**
+
+
+*
+
+
+* @author kolobj
+
+
+*/
+
+
+public interface CookingRepository extends PagingAndSortingRepository<Cooking, Long> {}
+
