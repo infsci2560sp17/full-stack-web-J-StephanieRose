43a1b9705cd1b9a82907511b64f0e2da8237428c
J Stephanie Rose <jsr67@pitt.edu>
2017-03-25 23:41:50 +0000

    added SportsRepository.java

---
+++ src/main/java/edu/infsci2560/repositories/SportsRepository.java
@@ -0,0 +1,43 @@
+
+/*
+
+
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+
+
+*/
+
+
+package edu.infsci2560.repositories;
+
+
+
+
+
+
+
+import edu.infsci2560.models.Sports;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+
+
+
+
+
+
+/**
+
+
+*
+
+
+* @author kolobj
+
+
+*/
+
+
+public interface SportsRepository extends PagingAndSortingRepository<Sports, Long> {}
+
