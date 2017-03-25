7a33e8e94877637da1f58d6cbb161a849a56ed44
J Stephanie Rose <jsr67@pitt.edu>
2017-03-25 23:39:27 +0000

    added artscraftsrepository.java

---
+++ src/main/java/edu/infsci2560/repositories/ArtscraftsRepository.java
@@ -0,0 +1,41 @@
+
+/*
+
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+
+
+*/
+
+
+package edu.infsci2560.repositories;
+
+
+
+
+
+
+
+import edu.infsci2560.models.Artscrafts;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+
+
+
+
+
+
+/**
+
+
+*
+
+
+* @author kolobj
+
+
+*/
+
+
+public interface ArtscraftsRepository extends PagingAndSortingRepository<Artscrafts, Long> {}
