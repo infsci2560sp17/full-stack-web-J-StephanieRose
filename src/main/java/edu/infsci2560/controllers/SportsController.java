433aa379481db578742cf66107ee5dc634bee2a9
J Stephanie Rose <jsr67@pitt.edu>
2017-03-26 00:36:48 +0000

    added SportsController.java

---
+++ src/main/java/edu/infsci2560/controllers/SportsController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.infsci2560.controllers;
+
+import edu.infsci2560.models.Sports;
+import edu.infsci2560.repositories.SportsRepository;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author kolobj
+ */
+@Controller
+public class SportsController {
+    @Autowired
+    private SportsRepository repository;
+    
+    @RequestMapping(value = "sports", method = RequestMethod.GET)
+    public ModelAndView index() {        
+        return new ModelAndView("sports", "sports", repository.findAll());
+    }
+    
+    @RequestMapping(value = "sports/add", method = RequestMethod.POST, consumes="application/x-www-form-urlencoded", produces = "application/json")
+    public ModelAndView create(@ModelAttribute @Valid Sports sports, BindingResult result) {
+        repository.save(sports);
+        return new ModelAndView("sports", "sports", repository.findAll());
+    }
+    
+}
