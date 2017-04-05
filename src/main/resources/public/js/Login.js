e0e2853c4f5e316f91adaf4d9de53944086a5eb6
J Stephanie Rose <jsr67@pitt.edu>
2017-04-05 14:45:10 +0000

    something is hindering login from going on its correct path

--- src/main/resources/public/js/Login.js
+++ src/main/resources/public/js/Login.js
@@ -1,17 +1,17 @@
 /* eslint-env node */
-import React from 'react'
-import withProfile from '../with-profile'
-import withLayout from '../with-layout'
-import LoggedInNotification from '../components/LoggedInNotification'
+//import React from 'react'
+//import withProfile from '../with-profile'
+//import withLayout from '../with-layout'
+//import LoggedInNotification from '../components/LoggedInNotification'
 
-<div id="fb-root"></div>
-<script>
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-</script>
+//<div id="fb-root"></div>
+//<script>
+//(function(d, s, id) {
+//  var js, fjs = d.getElementsByTagName(s)[0];
+ // if (d.getElementById(id)) return;
+ // js = d.createElement(s); js.id = id;
+ // js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+//  fjs.parentNode.insertBefore(js, fjs);
+//}(document, 'script', 'facebook-jssdk'));
+//</script>
 
