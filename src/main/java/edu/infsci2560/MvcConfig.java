2fa60c150d68c7e75a6935f4eb065afeccb5bfe7
J Stephanie Rose <jsr67@pitt.edu>
2017-04-22 23:39:00 +0000

    updated mvcConfig

--- src/main/java/edu/infsci2560/MvcConfig.java
+++ src/main/java/edu/infsci2560/MvcConfig.java
@@ -27,8 +27,8 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/sports").setViewName("sports");
         registry.addViewController("/volunteering").setViewName("volunteering");
         registry.addViewController("/about").setViewName("about");
-        registry.addViewController("/presentation").setViewName("presentation");
         registry.addViewController("/profile").setViewName("profile");
+        registry.addViewController("/logout").setViewName("Signout");
         registry.addViewController("/files").setViewName("uploadForm");
     }
 
